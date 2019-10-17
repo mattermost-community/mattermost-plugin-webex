256487f68b3fdc00a23d205c450a240257ed3bf8
Christopher Poile <cpoile@gmail.com>
2019-10-17 12:25:52 -0400

    1.0.1 version bump (#14)

--- server/manifest.go
+++ server/manifest.go
@@ -5,5 +5,5 @@ var manifest = struct {
 	Version string
 }{
 	Id:      "com.mattermost.webex",
-	Version: "1.0.0",
+	Version: "1.0.1",
 }
--- webapp/src/manifest.js
+++ webapp/src/manifest.js
@@ -1,2 +1,2 @@
 export const id = 'com.mattermost.webex';
-export const version = '1.0.0';
+export const version = '1.0.1';
