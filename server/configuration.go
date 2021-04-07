f2e9fd4312b16b88e104ce307cce956bd392d85d
Guilhem Bonnefille <guilhem.bonnefille@csgroup.eu>
2021-04-07 19:01:43 +0200

    Add a settings to control URL substitution
    This substitution is known to break Linux experience.
    While the raw URL is also known to work on Windows.
    
    In some circumstance, it could be desirable to not subtitute
    URLs.

--- server/configuration.go
+++ server/configuration.go
@@ -28,6 +28,8 @@ import (
 type configuration struct {
 	SiteHost string `json:"sitehost"`
 
+	UrlConversion bool `json:"UrlConversion"`
+
 	// siteName is the SiteHost up to .webex.com
 	// Eg., for testsite.my.webex.com, siteName would be: testsite.my
 	siteName string
