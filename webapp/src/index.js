7b918083aff0de98b6ad19dc48df9d9322b1db40
Maisnam Raju Singh <85172229+maisnamrajusingh@users.noreply.github.com>
2021-07-09 07:04:55 +0700

    Update index.js

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,8 +3,9 @@
 
 import React from 'react';
 
-import {id as pluginId} from './manifest';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
+
+import {id as pluginId} from './manifest';
 
 import Icon from './components/icon.jsx';
 import PostTypeWebex from './components/post_type_webex';
